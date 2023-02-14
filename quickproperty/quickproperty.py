8edb473f5f858aee9d9ef4c3f29ee0a67b00ddac
spapa013 <stelios@spapa.us>
2023-02-14 11:06:02 -0600

    on_get_set_kws passed as defaults

--- quickproperty/quickproperty.py
+++ quickproperty/quickproperty.py
@@ -25,7 +25,7 @@ class QuickProperty:
             on_get_kws_name (str) - the name of the attr in the owner class where the kws to pass to on_get are stored
             on_set_kws_name (str) - the name of the attr in the owner class where the kws to pass to on_Set are stored
             static (bool) - if True, does not pass the owner class or instance into on_get and on_set, default is False
-            on_get_set_kws - additional kws to pass to both on_set and on_get
+            on_get_set_kws - additional default kws to pass to both on_set and on_get. 
         """
         assert not (on_get_kws is not None and on_get_kws_name is not None), 'Provide either on_get_kws or on_get_kws_name but not both'
         assert not (on_set_kws is not None and on_set_kws_name is not None), 'Provide either on_set_kws or on_set_kws_name but not both'
@@ -38,7 +38,7 @@ class QuickProperty:
         self.on_set_kws_name = on_set_kws_name
         self.static = static
         self.on_get_set_kws = on_get_set_kws
-        
+
     def __set_name__(self, owner, name):
         self.public_name = name
         self.private_name = '_' + name
@@ -50,6 +50,7 @@ class QuickProperty:
 
         if self.f_on_get is not None:
             if self.on_get_kws_name is not None:
+                print('got here')
                 on_get_kws = getattr(source, self.on_get_kws_name)
             elif self.on_get_kws is not None:
                 assert isinstance(self.on_get_kws, dict)
@@ -57,7 +58,8 @@ class QuickProperty:
             else:
                 on_get_kws = {}
             assert isinstance(on_get_kws, dict)
-            on_get_kws.update(self.on_get_set_kws)
+            for k, v in self.on_get_set_kws.items():
+                on_get_kws.setdefault(k, v)
             return self.f_on_get(source, value, **on_get_kws) if not self.static else self.f_on_get(value, **on_get_kws)
         else:
             return value
@@ -71,7 +73,8 @@ class QuickProperty:
             else:
                 on_set_kws = {}
             assert isinstance(on_set_kws, dict)
-            on_set_kws.update(self.on_get_set_kws)
+            for k, v in self.on_get_set_kws.items():
+                on_set_kws.setdefault(k, v)
             value = self.f_on_set(instance, value, **on_set_kws) if not self.static else self.f_on_set(value, **on_set_kws)
     
         setattr(instance, self.private_name, value)
